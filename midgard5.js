2373314382416caf5eda3aeabbc22f62e5276dbf
hjmaier <51607746+hjmaier@users.noreply.github.com>
2021-02-13 14:24:41 +0100

    V0.04

--- midgard5.js
+++ midgard5.js
@@ -4,8 +4,12 @@ import m5CharacterSheet from "./module/sheets/m5CharacterSheet.js";
 Hooks.once("init", function () {
   console.log("M5 | Initialisierung Midgard 5");
 
+// Default Sheet für Items definieren und das Standardsheet deaktivieren
+
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("midgard5", m5ItemSheet, { makeDefault: true });
+
+// Default Sheet für Actors definieren und das Standardsheet deaktivieren
 
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("midgard5", m5CharacterSheet, { makeDefault: true });
